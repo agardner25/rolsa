b4c685569b09c43283b3d6720162fa7bff1e1718
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-02-09 11:46:25 +0000

    add logout

---
+++ logout.php
@@ -0,0 +1,25 @@
+<?php include ('server.php');
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <title>Log Out</title>
+</head>
+<body>
+<?php include ('navbar.php') ?>
+<div class="main-container">
+    <div class="container">
+        <h1>You have been successfully logged out.</h1><br>
+        <h2><a href="login.php">Log In </a></h2>
+    </div>
+</div>
+<div class="footer">
+    <p>Footer</p>
+</div>
+</body>
+</html>
