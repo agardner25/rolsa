055147c1746c6a2ceada813edbb4a9e6af682300
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-02-09 12:09:04 +0000

    fixes

---
+++ bookings.php
@@ -0,0 +1,17 @@
+<?php include('include/server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bookings - Rolsa Technologies</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php include('include/navbar.php') ?>
+<div class="container">
+    <h1>Services and Consultations</h1>
+
+</div>
+</body>
+</html>
--- include/navbar.php
+++ include/navbar.php
@@ -5,9 +5,9 @@ if ($_SESSION['username'] == '') : ?>
     <nav class="navbar">
         <ul class="nav-list">
             <li><a href="index.php"><h1 class="logo">Rolsa</h1></a></li>
-            <li><a href="index.php">Green products</a></li>
-            <li><a href="index.php">Carbon footprint</a></li>
-            <li><a href="index.php">Services and consultations</a></li>
+            <li><a href="products.php">Green products</a></li>
+            <li><a href="calculator.php">Carbon footprint</a></li>
+            <li><a href="bookings.php">Services and consultations</a></li>
             <li><a href="login.php">Log in</a></li>
             <li><a href="signup.php">Sign up</a></li>
         </ul>
@@ -18,9 +18,9 @@ if ($_SESSION['username'] == '') : ?>
     <nav class="navbar">
         <ul class="nav-list">
             <li><a href="index.php"><h1 class="logo">Rolsa</h1></a></li>
-            <li><a href="index.php">Green Products</a></li>
-            <li><a href="index.php">Carbon footprint</a></li>
-            <li><a href="index.php">Services and consultations</a></li>
+            <li><a href="products.php">Green Products</a></li>
+            <li><a href="calculator.php">Carbon footprint</a></li>
+            <li><a href="bookings.php">Services and consultations</a></li>
             <li><a href="index.php">My dashboard</a></li>
             <li><a href="logout.php">Log Out</a></li>
         </ul>
