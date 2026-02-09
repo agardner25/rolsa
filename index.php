10f75dcd4183f25720e06d37c17a98b987be4433
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-02-09 11:21:09 +0000

    fix css link

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rolsa Technologies</title>
-    <rel link="stylesheet" href="main.css";>
+    <link rel="stylesheet" href="main.css";>
 </head>
 <body>
 <?php include('navbar.php') ?>
