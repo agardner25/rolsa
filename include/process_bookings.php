163c108834b5dbbcadd60b4cac2dafd28c4eb202
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-02-09 14:00:49 +0000

    fixes

--- include/process_bookings.php
+++ include/process_bookings.php
@@ -13,12 +13,12 @@ if (isset($_POST['book_service'])) {
     $booking_date = $_POST['booking_date'] ?? '';
     
     if (empty($user_id)) { array_push($errors, "You must be logged in to book"); }
-    if (empty($service)) { array_push($errors, "Service is required"); }
+    if (empty($service_id)) { array_push($errors, "Service is required"); }
     if (empty($booking_date)) { array_push($errors, "Booking date is required"); }
     
     // Runs if there are no errors
     if (count($errors) == 0) {
-        $query_insert_booking = "INSERT INTO bookings (service_id, user_id, 'time') 
+        $query_insert_booking = "INSERT INTO bookings (service_id, user_id, booking_time) 
                 VALUES('$service_id', '$user_id', '$booking_date')";
         
         // Runs insertion query
