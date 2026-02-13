f990020aadd28948533b42a425228e9b0b909307
agardner25 <a.gardner@stud.abingdon-witney.ac.uk>
2026-02-13 15:07:09 +0000

    products

---
+++ products.php
@@ -0,0 +1,16 @@
+<?php include('include/server.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rolsa Technologies</title>
+    <link rel="stylesheet" href="css/main.css";>
+</head>
+<body>
+<?php include('include/navbar.php') ?>
+<div class="container">
+    <h1>Welcome back, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h1>
+</div>
+</body>
+</html>
